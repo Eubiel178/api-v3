1b3868aab679e4e45cf5a51fb0bca8e372bba2bc
Gabriel da Silva Santos <gabrielsil40028922@gmail.com>
2025-08-19 21:04:05 -0300

    Add files via upload

--- src/routes/reminderControl.js
+++ src/routes/reminderControl.js
@@ -2,6 +2,8 @@ const ReminderModel = require("../models/reminder.model");
 const { Router } = require("express");
 
 const router = Router();
+const SubscriptionModel = require("../models/subscription.model");
+const webPush = require("web-push");
 
 // Criar novo lembrete
 router.post("/reminders", async (req, res) => {
@@ -16,5 +18,50 @@ router.post("/reminders", async (req, res) => {
     res.status(500).json({ status: "ERROR", errorMessage: error.message });
   }
 });
+
+router.post("/send-reminders", async (req, res) => {
+  try {
+    console.log("🔔 Checando lembretes do dia...");
+    const hojeStr = new Date().toISOString().slice(0, 10);
+
+    const lembretes = await ReminderModel.find({
+      remindAt: hojeStr,
+      notificado: false,
+    });
+
+    for (const lembrete of lembretes) {
+      const sub = await SubscriptionModel.findOne({ userID: lembrete.userID });
+      if (!sub) continue;
+
+      try {
+        await webPush.sendNotification(
+          sub.subscription,
+          JSON.stringify({
+            title: lembrete?.title || "Lembrete",
+            message: lembrete?.description,
+          })
+        );
+
+        lembrete.notificado = true;
+        await lembrete.save();
+        console.log("Notificado:", lembrete.title);
+      } catch (err) {
+        if (err.statusCode === 410 || err.statusCode === 404) {
+          console.log(`Subscription expirada ou removida. Deletando...`);
+          await SubscriptionModel.deleteOne({ userID: lembrete.userID });
+        } else {
+          console.error("Erro ao enviar notificação:", err);
+        }
+      }
+    }
+
+    res.status(200).json({ ok: true, enviados: lembretes.length });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ ok: false, error: err.message });
+  }
+});
+
+module.exports = router;
 
 module.exports = router;
