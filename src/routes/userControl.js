fd07ab61ffc22192ff24c69b56b779fedbcec1dd
Gabriel da Silva Santos <gabrielsil40028922@gmail.com>
2025-08-20 11:57:10 -0300

    Update userControl.js

--- src/routes/userControl.js
+++ src/routes/userControl.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const UserModel = require("../models/user.model");
+const ReminderModel = require("../models/reminder.model");
+const SubscriptionModel = require("../models/subscription.model");
 
 const router = Router();
 
@@ -71,6 +73,58 @@ router.get("/users/:id", async (request, response) => {
     }
   } catch (error) {
     response.status(500).json({ status: "ERROR", errorMessage: error.message });
+  }
+});
+
+router.post("/reminders", async (req, res) => {
+  try {
+    const { body } = req;
+
+    const reminder = await ReminderModel.create(body);
+
+    return res.status(200).json({ status: "OK", reminder });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "ERROR", errorMessage: error.message });
+  }
+});
+
+router.post("/save-subscription", async (req, res) => {
+  const { userID, subscription } = req.body;
+  if (!userID || !subscription) return res.status(400).send("Dados inválidos");
+
+  try {
+    // busca a subscription existente
+    const existing = await SubscriptionModel.findOne({ userID });
+
+    // compara as subscriptions
+    const isSame =
+      existing &&
+      JSON.stringify(existing.subscription) === JSON.stringify(subscription);
+    console.log(isSame, "issame");
+
+    if (isSame) {
+      // já está salva, não precisa atualizar
+      console.log("Subscription já existente e válida:", existing);
+      return res.send({
+        success: true,
+        subscription: existing,
+        message: "Subscription já válida",
+      });
+    }
+
+    // se não existir ou for diferente, cria/atualiza
+    const saved = await SubscriptionModel.findOneAndUpdate(
+      { userID },
+      { subscription },
+      { upsert: true, new: true }
+    );
+
+    console.log("Subscription salva/atualizada:", saved);
+    res.send({ success: true, subscription: saved });
+  } catch (err) {
+    console.error("Erro ao salvar subscription:", err);
+    res.status(500).send({ success: false, error: err.message });
   }
 });
 
