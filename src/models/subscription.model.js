e41f161219d5d4350e5ac10b900928188ec33db9
Gabriel da Silva Santos <gabrielsil40028922@gmail.com>
2025-08-20 11:45:03 -0300

    Delete src directory

--- src/models/subscription.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const subscriptionSchema = new mongoose.Schema({
-  userID: { type: String, required: true, unique: true },
-  subscription: { type: Object, required: true },
-});
-
-module.exports = mongoose.model("subscriptions", subscriptionSchema);
