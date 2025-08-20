c107b984e1ae7094b7e7bd3e30f9d0e0698f1155
Gabriel da Silva Santos <gabrielsil40028922@gmail.com>
2025-08-20 08:22:44 -0300

    Add files via upload

--- src/routes/reminderControl.js
+++ src/routes/reminderControl.js
@@ -63,4 +63,54 @@ router.post("/reminders", async (req, res) => {
   }
 });
 
+router.get("/reminders", async (req, res) => {
+  try {
+    await connectToDatabase(); // garante conexão antes de qualquer find/create
+
+    // Criar novo lembrete
+
+    // Enviar lembretes do dia
+    console.log("🔔 Checando lembretes do dia...");
+    const hojeStr = new Date().toISOString().slice(0, 10);
+
+    const lembretes = await ReminderModel.find({
+      remindAt: hojeStr,
+      notificado: false,
+    });
+
+    for (const lembrete of lembretes) {
+      const sub = await SubscriptionModel.findOne({
+        userID: lembrete.userID,
+      });
+      if (!sub) continue;
+
+      try {
+        await webPush.sendNotification(
+          sub.subscription,
+          JSON.stringify({
+            title: lembrete?.title || "Lembrete",
+            message: lembrete?.description,
+          })
+        );
+
+        lembrete.notificado = true;
+        await lembrete.save();
+        console.log("Notificado:", lembrete.title);
+      } catch (err) {
+        if (err.statusCode === 410 || err.statusCode === 404) {
+          console.log(`Subscription expirada ou removida. Deletando...`);
+          await SubscriptionModel.deleteOne({ userID: lembrete.userID });
+        } else {
+          console.error("Erro ao enviar notificação:", err);
+        }
+      }
+    }
+
+    return res.status(200).json({ ok: true, enviados: lembretes.length });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "ERROR", errorMessage: error.message });
+  }
+});
+
 module.exports = router;
